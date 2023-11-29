0809cca4e0d06cbd43a7d8b48efc33e11283fce8
shahadkariyambath <shahadchikku@gmail.com>
2023-11-29 21:01:42 +0000

    duplicate emoji diplayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-const gameEmoji = ["❤", "🎶", "🎂", "🎁", "❤", "🎶", "🎂", "🎁"];
+const gameEmoji = ["❤", "🎶", "🎂", "🎁"];
 
 function App() {
+  const [box, setBox] = useState([]);
+
   const startGame = () => {
     const duplicateEmoji = [...gameEmoji, ...gameEmoji];
-    console.log(duplicateEmoji);
+    const newEmojiDuplicate = [];
+
+    while (newEmojiDuplicate.length < gameEmoji.length * 2) {
+      let index = Math.floor(Math.random() * duplicateEmoji.length);
+      newEmojiDuplicate.push({
+        emoji: duplicateEmoji[index],
+        position: newEmojiDuplicate.length,
+        solved: false,
+        flipped: false,
+      });
+
+      duplicateEmoji.splice(index, 1);
+    }
+    console.log(newEmojiDuplicate);
+    setBox(newEmojiDuplicate);
   };
 
   useEffect(() => {
@@ -18,11 +34,11 @@ function App() {
     <main>
       <h1>Memory Game in React</h1>
       <div className="container">
-        {gameEmoji.map((data, index) => (
+        {box.map((data, index) => (
           <div className="flip-card" key={index}>
             <div className="flip-card-inner">
               <div className="flip-card-front" />
-              <div className="flip-card-back">{data}</div>
+              <div className="flip-card-back">{data.emoji}</div>
             </div>
           </div>
         ))}
